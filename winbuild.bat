e704e61645c16d54ce1cd63e7664964de8e62208
Brandon Stansbury <brandonrstansbury@gmail.com>
2017-03-06 04:53:51 +0000

    split up cflags line

--- winbuild.bat
+++ winbuild.bat
@@ -4,7 +4,8 @@ SETLOCAL
 SET build_dir=build
 
 SET cc=clang
-SET cflags=-Wall -Wextra -Werror -pedantic -Wno-deprecated-declarations -Wno-unused-parameter -Wno-gnu-zero-variadic-macro-arguments -std=c11 -Os -iquoteCinyTest -v
+SET cflags=-Wall -Wextra -Werror -pedantic -Wno-deprecated-declarations -Wno-unused-parameter ^
+	-Wno-gnu-zero-variadic-macro-arguments -std=c11 -Os -iquoteCinyTest -v
 SET ldflags=-LCinyTest
 SET ldlibs=-lcinytest
 
