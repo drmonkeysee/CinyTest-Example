2bb3f53c966b3bb2995633edb73d311070cd64ff
Brandon Stansbury <brandonrstansbury@gmail.com>
2017-03-06 05:08:40 +0000

    inline library options

--- winbuild.bat
+++ winbuild.bat
@@ -6,13 +6,11 @@ SET build_dir=build
 SET cc=clang
 SET cflags=-Wall -Wextra -Werror -pedantic -Wno-deprecated-declarations -Wno-unused-parameter ^
 	-Wno-gnu-zero-variadic-macro-arguments -std=c11 -Os -iquoteCinyTest -v
-SET ldflags=-LCinyTest
-SET ldlibs=-lcinytest
 
 RMDIR /S /Q %build_dir%
 
 MKDIR %build_dir%
-%cc% %cflags% src\*.c %ldflags% %ldlibs% -o %build_dir%\recttests.exe
+%cc% %cflags% src\*.c -LCinyTest -lcinytest -o %build_dir%\recttests.exe
 
 ENDLOCAL
 EXIT /B %ERRORLEVEL%
