c294b8f624da9e77c0e02a980f2ab0ea34315881
Brandon Stansbury <brandonrstansbury@gmail.com>
2020-09-12 15:40:03 -0700

    update winbuild to use current dir for include/lib

--- winbuild.bat
+++ winbuild.bat
@@ -6,12 +6,12 @@ SET build_dir=build
 SET cc=clang
 SET cflags=-Wall -Wextra -Werror -pedantic -Wno-deprecated-declarations ^
 	-Wno-unused-parameter -Wno-gnu-zero-variadic-macro-arguments -std=c17 ^
-	-Os -iquoteCinyTest -v
+	-Os -iquote. -v
 
 RMDIR /S /Q %build_dir%
 
 MKDIR %build_dir%
-%cc% %cflags% src\*.c -LCinyTest -lcinytest -o %build_dir%\recttests.exe
+%cc% %cflags% src\*.c -L. -lcinytest -o %build_dir%\recttests.exe
 
 ENDLOCAL
 EXIT /B %ERRORLEVEL%
